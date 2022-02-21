4e8c26a14e0e56fee2b5af726e5ad33a92bc6bfc
CaioSilva-hub <caio.csbb@gmail.com>
2022-02-21 12:53:50 -0300

    exercio estrutura de dados

---
+++ estrutura.js
@@ -0,0 +1,27 @@
+const boolean = true
+const string = "Felipe"
+const number = 10
+const float = 10.1
+const sem_resultado = undefined
+const nula = null
+const array = [] // Lista
+const objeto = {}  
+
+const celulas = [
+    {
+        lider: "Fulano",
+        numero_celula: 10,
+        mais_de_10: true,
+        membros: ["abreu"]
+    },
+    {
+        lider: "Fulano 2",
+        numero_celula: 11,
+        mais_de_10: false,
+        membros: ["joao", "maria", "zezinho"]
+    }
+]
+
+//Exercicio
+
+// 1 - Lista, 3 Objetos, 7 - dados, 3 string, 2 bool, 1 number, 1 obejto(2 dados, strings) 
